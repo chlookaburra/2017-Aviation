12f531f767950d6a1c8ea836ceb89c3a8b55875a
chlookaburra <31208672+chlookaburra@users.noreply.github.com>
2017-08-30 19:08:06 +0900

    august 30, 2017 upload final ornithopter

---
+++ finalServoOrnithopter.ino
@@ -0,0 +1,49 @@
+/* Sweep
+  by BARRAGAN <http://barraganstudio.com>
+  This example code is in the public domain.
+
+  modified 8 Nov 2013
+  by Scott Fitzgerald
+  http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+#include <Servo.h>
+
+const int rightWingPin = 5;
+const int leftWingPin = 3;
+
+Servo rightWing;
+Servo leftWing;
+unsigned long previousMillis = 0;
+const long interval = 100;
+int counter = 0;
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  rightWing.attach(rightWingPin);  // attaches the servo on pin 9 to the servo object
+  rightWing.write(90);
+
+  leftWing.attach(leftWingPin);  // attaches the servo on pin 9 to the servo object
+  leftWing.write(90);
+}
+
+void loop() {
+
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+    if (counter % 2 == 0) {
+      rightWing.write(30)fest;
+      leftWing.write(150);
+      counter++;
+    } else {
+      rightWing.write(160);
+      leftWing.write(30);
+      counter++;
+    }
+  }
+
+
+}
